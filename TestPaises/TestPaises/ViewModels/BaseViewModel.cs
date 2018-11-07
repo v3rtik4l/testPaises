fd7786c8cef75013b6e07dfc24a3ce9511cd7685
Jonatan Medina Perez <jmperez9@gmail.com>
2018-11-07 21:46:11 +0100

    Login View Model with base view model

---
+++ TestPaises/TestPaises/ViewModels/BaseViewModel.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+namespace TestPaises.ViewModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+
+    public class BaseViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                return;
+            }
+
+            backingField = value;
+            OnPropertyChanged(propertyName);
+        }
+
+    }
+}
--- TestPaises/TestPaises/ViewModels/LoginViewModel.cs
+++ TestPaises/TestPaises/ViewModels/LoginViewModel.cs
@@ -2,28 +2,105 @@
 
 namespace TestPaises.ViewModels
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+
+        #region Atributes
+        private string password;
+        private string email;
+        private bool isRunning;
+        private bool isEnabled;
+
+        #endregion
+
         #region Properties
         public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsEnabled {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+
         public bool IsRemembered { get; set; }
         #endregion
 
         #region Comamnds
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Password",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "jmperez9@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Fuck yeah",
+                    "Accept");
+            return;
+
+        }
         #endregion
 
         #region Constructor
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
     }
